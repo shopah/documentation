e2ee6733b7ce3d3fec18973061d018ee3c955b17
Lexacle Technologies <lexacletechnologies@gmail.com>
2022-03-10 22:23:11 +0300

    deploy

---
+++ assets/index.html.39d11f54.js
@@ -0,0 +1,1 @@
+const e={key:"v-8daa1a0e",path:"/",title:"Introduction",lang:"en-US",frontmatter:{lang:"en-US",title:"Introduction",description:"Project Documentation Guidelines"},excerpt:"",headers:[],git:{updatedTime:1646936033e3,contributors:[{name:"Lexacle Technologies",email:"lexacletechnologies@gmail.com",commits:1}]},filePathRelative:"README.md"};export{e as data};
